2e23f2a318a4abf79e7bc18990d5dade625750f3
joseph <joseph.gargari@gmail.com>
2024-09-19 21:56:57 +0200

    fix returntypes to DTO

--- backend/FrisbeeGolfer.api/Interfaces/IPlayerRepository.cs
+++ backend/FrisbeeGolfer.api/Interfaces/IPlayerRepository.cs
@@ -5,8 +5,8 @@ namespace FrisbeeGolfer.api.Interfaces;
 
 public interface IPlayerRepository
 {
-    public Task<List<Player>> GetPlayersAsync();
-    public Task<Player> GetPlayerAsync(string playerId);
+    public Task<List<PlayerDto>> GetPlayersAsync();
+    public Task<PlayerDto> GetPlayerAsync(string playerId);
     public Task SavePlayerAsync(PlayerDto player);
     public Task DeletePlayersAsync();
     public Task DeletePlayerAsync(string playerId);
